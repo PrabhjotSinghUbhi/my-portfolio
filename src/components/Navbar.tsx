eca1c88fee9251974e8d49448291f92539acc88b
Prabhjot Singh <prabh.ubhi7042@gmail.com>
2025-09-19 19:55:41 +0530

    feat: update portfolio with new SVG icons and enhance meta tags for SEO

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,7 +8,10 @@ function NavbarMobile() {
     return (
         <aside className='fixed border-b-1 border-neutral-500 top-0 left-0 right-0 h-16  bg-blend-luminosity text-3xl flex justify-between items-center px-6 py-3 z-50 backdrop-blur-md '>
             <section id='logo' className='font-extrabold gradient-text text-black dark:text-white '>
-                &DEV
+                <span>
+                    <img src="/black_braces.svg" alt="" />
+                </span>
+                DEV
             </section>
             <section id='Menu' className='dark:text-white text-black'>
                 <SheetMobile open={open} setOpen={setOpen} />
