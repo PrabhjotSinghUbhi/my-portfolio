bdef8747ad5a06f500f96a4498c905540d8c0c6a
Prabhjot Singh <prabh.ubhi7042@gmail.com>
2025-09-20 08:58:34 +0530

    fix: add alt text to logo image in NavbarMobile component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,7 +8,7 @@ function NavbarMobile() {
     return (
         <aside className='fixed border-b-1 border-neutral-500 top-0 left-0 right-0 h-16  bg-blend-luminosity text-3xl flex justify-between items-center px-6 py-3 z-50 backdrop-blur-md '>
             <section id='logo' className='font-extrabold flex items-center gap-4 gradient-text text-black dark:text-white '>
-                <img src="/grp1.png" alt="" className='' height={40} width={40} /> DEV
+                <img src="/grp1.png" alt="group 1" className='' height={40} width={40} /> DEV
             </section>
             <section id='Menu' className='dark:text-white text-black'>
                 <SheetMobile open={open} setOpen={setOpen} />
