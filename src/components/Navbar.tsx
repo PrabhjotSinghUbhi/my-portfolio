9ed47b22a627bedda72e99aadb3e7cd683fc8bbe
Prabhjot Singh <prabh.ubhi7042@gmail.com>
2025-09-20 08:47:13 +0530

    feat: enhance NavbarMobile layout and add Badge component to RecentContributionCards

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,11 +7,8 @@ function NavbarMobile() {
 
     return (
         <aside className='fixed border-b-1 border-neutral-500 top-0 left-0 right-0 h-16  bg-blend-luminosity text-3xl flex justify-between items-center px-6 py-3 z-50 backdrop-blur-md '>
-            <section id='logo' className='font-extrabold gradient-text text-black dark:text-white '>
-                <span>
-                    <img src="/grp1.png" alt="Braces logo" />
-                </span>
-                DEV
+            <section id='logo' className='font-extrabold flex items-center gap-4 gradient-text text-black dark:text-white '>
+                <img src="/grp1.png" alt="" className='' height={40} width={40} /> DEV
             </section>
             <section id='Menu' className='dark:text-white text-black'>
                 <SheetMobile open={open} setOpen={setOpen} />
